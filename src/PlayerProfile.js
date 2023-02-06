705941afdc9481d6fff88683d33e3c3f9a823e37
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-02-06 17:57:31 -0500

    updated sizes

--- src/PlayerProfile.js
+++ src/PlayerProfile.js
@@ -3,9 +3,9 @@ import scroll from './assets/playerscroll.svg'
 
 export const PlayerProfile = ({ user, gold, xp, rank }) => {
 	return (
-		<div className='flex justify-center items-center w-screen h-screen'>
+		<div className='flex justify-center items-center w-[40rem] h-[40rem]'>
 			<img src={scroll} className='w-[40rem] h-[40rem] mx-auto py-2.5 text-4xl text-center font-extrabold p-auto flex justify-center items-center' alt='scroll for profile'></img>
-			<div className='absolute w-screen h-auto mx-auto inset-x-0 text-4xl text-center font-extrabold text-gray-500 uppercase flex justify-center items-center'>
+			<div className='absolute w-auto h-auto text-4xl text-center font-extrabold text-gray-500 uppercase flex justify-center items-center'>
 				Player Profile
 				<br />
 				<br />
