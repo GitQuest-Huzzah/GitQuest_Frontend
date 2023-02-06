aba1c14ca9513ce3f0f98e576ef45356054e94f6
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-02-06 17:46:15 -0500

    removed unnecessary css

--- src/PlayerProfile.js
+++ src/PlayerProfile.js
@@ -4,7 +4,7 @@ import scroll from './assets/playerscroll.svg';
 export const PlayerProfile = ({ user, gold, xp, rank }) => {
 	return (
 		<div className='flex justify-center items-center w-screen h-screen'>
-			<img src={scroll} className='w-[40rem] h-[40rem] mx-auto py-2.5 bottom-1/3 inset-x-0 text-4xl text-center font-extrabold p-auto flex justify-center items-center' alt='scroll for profile'></img>
+			<img src={scroll} className='w-[40rem] h-[40rem] mx-auto py-2.5 text-4xl text-center font-extrabold p-auto flex justify-center items-center' alt='scroll for profile'></img>
 			<div className='absolute w-screen h-auto mx-auto inset-x-0 text-4xl text-center font-extrabold text-gray-500 uppercase flex justify-center items-center'>
 				Player Profile
 				<br />
