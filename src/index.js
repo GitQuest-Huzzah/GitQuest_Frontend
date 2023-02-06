2482ac1ca9278c790823d307d1cb5d8b8436f680
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-02-06 15:05:10 -0500

    updated, removed redux

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
-import store from './store';
 
 const root = createRoot(document.getElementById('app'));
 root.render(
 	<Router>
-		<Provider store={store}>
 			<App />
-		</Provider>
 	</Router>
 );
 
