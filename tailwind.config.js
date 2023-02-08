64ed41daf0dea4f6358092cf3f43a9408f8efb4f
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-02-08 14:34:17 -0500

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  darkMode: 'class',
-}
+	content: [
+		'./src/**/*.html',
+		'./src/**/*.vue',
+		'./src/**/*.jsx',
+		'./src/**/*.js',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				doom: ['"MedievalTimes"', 'sans-serif'],
+                medieval: ['"MedievalTimes"', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+	darkMode: 'class',
+    fontPath: './fonts',
+};
