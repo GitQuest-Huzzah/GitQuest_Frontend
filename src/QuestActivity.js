23c3d272cd279b70b13e8502a8b22c78eb035887
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-12 03:38:07 -0500

    tweaking graph

--- src/QuestActivity.js
+++ src/QuestActivity.js
@@ -9,13 +9,13 @@ export const QuestActivity = ({ activityStats }) => {
 
 	const width = 500;
 	const height = 300;
-	const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+	const margin = { top: 0, bottom: 55, left: 50, right: 50 };
 	const drawChart = () => {
 		const svg = d3
 			.select(".chart")
 			.append("svg")
-			// .attr("height", height - margin.top - margin.bottom)
-			// .attr("width", width - margin.left - margin.right)
+			.attr("height", height - margin.top - margin.bottom)
+			.attr("width", width - margin.left - margin.right)
 			.attr("viewBox", [0, 0, width, height])
 			.style('border', '4px solid crimson')
 			.style('border-radius','10px')
@@ -25,7 +25,7 @@ export const QuestActivity = ({ activityStats }) => {
 			.attr('width',1500)
 			.attr('height', 500)
 			.attr('x', -500)
-			
+
 		const x = d3.scaleBand()
 			.domain(d3.range(activityStats.length))		
 			.range([margin.left, width - margin.right])
@@ -39,7 +39,6 @@ export const QuestActivity = ({ activityStats }) => {
 			.append('g')
 			.attr('fill', 'crimson')
 			.selectAll('rect')
-			.style('border-radius', '10px')
 			.data(activityStats.sort((a,b) => d3.descending(a.questQuantity, b.questQuantity)))
 			.join('rect')
 			.attr('x', (d,i) => x(i))
@@ -50,14 +49,14 @@ export const QuestActivity = ({ activityStats }) => {
 		const xAxis = (g) => {
 			g.attr('transform', `translate(0, ${height - margin.bottom})`)
 			.call(d3.axisBottom(x).tickFormat(i => activityStats[i].name))
-			.attr('font-size', '20px')
-			.attr('font-family', 'Medieval Times')
+			.attr('font-size', '0.75em')
+			.attr('font-family', 'MedievalTimes')
 		}
 		const yAxis = (g) => {
 			g.attr('transform', `translate(${margin.left},0)`)
 			.call(d3.axisLeft(y).ticks(null, activityStats.format))
 			.attr('font-size', '20px')
-			.attr('font-family', 'Medieval Times')
+			.attr('font-family', 'MedievalTimes')
 		}
 		svg.append('g').call(xAxis);
 		svg.append('g').call(yAxis);
