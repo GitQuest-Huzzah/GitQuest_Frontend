e65f00fadfc5a337d49cab2150549e2105a0b332
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-12 03:11:49 -0500

    tweaking graph

--- src/QuestActivity.js
+++ src/QuestActivity.js
@@ -1,41 +1,66 @@
 import React, { useEffect } from "react";
+import background from './assets/stonebackground.svg';
 import * as d3 from "d3";
 export const QuestActivity = ({ activityStats }) => {
+	console.log(activityStats)
 	useEffect(() => {
 		drawChart();
-	});
+	},[]);
+
+	const width = 500;
+	const height = 300;
+	const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 	const drawChart = () => {
-		const data = activityStats;
-		const labels = data.map((entry) =>
-			entry.name.concat(": " + entry.questQuantity)
-		);
-		const totals = data.map((entry) => entry.questQuantity);
 		const svg = d3
 			.select(".chart")
 			.append("svg")
-			.attr("width", totals.length * 110)
-			.attr("height", totals.length * 100)
-			.style("border", "1px solid white");
+			// .attr("height", height - margin.top - margin.bottom)
+			// .attr("width", width - margin.left - margin.right)
+			.attr("viewBox", [0, 0, width, height])
+			.style('border', '4px solid crimson')
+			.style('border-radius','10px')
+
+		svg.append('image')
+			.attr('xlink:href', background)
+			.attr('width',1500)
+			.attr('height', 500)
+			.attr('x', -500)
+			
+		const x = d3.scaleBand()
+			.domain(d3.range(activityStats.length))		
+			.range([margin.left, width - margin.right])
+			.padding(0.1);
+
+		const y = d3.scaleLinear()
+			.domain([0,15])
+			.range([height - margin.bottom, margin.top]);
 
 		svg
-			.selectAll("rect")
-			.data(totals)
-			.enter()
-			.append("rect")
-			.attr("x", (d, i) => i * (40 * totals.length))
-			.attr("y", (d, i) => 300 - 10 * d)
-			.attr("width", 95)
-			.attr("height", (d, i) => d * 10)
-			.style("fill", "goldenrod");
-		svg
-			.selectAll("text")
-			.style("fill", "white")
-			.data(labels)
-			.enter()
-			.append("text")
-			.text((d) => d)
-			.attr("x", (d, i) => i * (40 * totals.length))
-			.attr("y", (d, i) => 300 - 5);
+			.append('g')
+			.attr('fill', 'crimson')
+			.selectAll('rect')
+			.style('border-radius', '10px')
+			.data(activityStats.sort((a,b) => d3.descending(a.questQuantity, b.questQuantity)))
+			.join('rect')
+			.attr('x', (d,i) => x(i))
+			.attr('y', (d) => y(d.questQuantity))
+			.attr('height', d => y(0) - y(d.questQuantity))
+			.attr('width', x.bandwidth())
+
+		const xAxis = (g) => {
+			g.attr('transform', `translate(0, ${height - margin.bottom})`)
+			.call(d3.axisBottom(x).tickFormat(i => activityStats[i].name))
+			.attr('font-size', '20px')
+			.attr('font-family', 'Medieval Times')
+		}
+		const yAxis = (g) => {
+			g.attr('transform', `translate(${margin.left},0)`)
+			.call(d3.axisLeft(y).ticks(null, activityStats.format))
+			.attr('font-size', '20px')
+			.attr('font-family', 'Medieval Times')
+		}
+		svg.append('g').call(xAxis);
+		svg.append('g').call(yAxis);
 	};
 	return <div className={"chart"}></div>;
 };
