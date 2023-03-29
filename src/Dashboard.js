e77d704dd667963cbf5e5015dc2dc847f0667ada
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-03-29 17:41:40 -0400

    token

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,19 +1,20 @@
-import React, {useEffect} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+export const Dashboard = ({ token }) => {
+  const [user, setUser] = useState({})
+	const fetchData = async () => {
+		const response = await axios.get(
+			"https://gitgoingslackbot.uc.r.appspot.com/api/auth/me",
+			{ headers: { authorization: token } }
+		);
+		console.log(response);
+    setUser(response)
+	};
 
-export const Dashboard = () => {
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  const fetchData = async() => {
-    const response = await axios.get('https://gitgoingslackbot.uc.r.appspot.com/api/auth/me');
-    console.log(response)
-  }
- 
-  useEffect(()=>{
-    fetchData()
-  }, [])
-
-  return (
-    <div>Dashboard</div>
-  )
-}
+	return <div>{user}</div>;
+};
