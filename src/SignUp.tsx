46dbb1612407ab37006d0780963bbbb0192a7a5b
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-04-06 14:14:49 -0400

    loading/ login to signup:

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -8,14 +8,14 @@ import {
 } from "./cssClasses";
 import axios from "axios";
 import { LoginProps } from "./Interfaces";
-export const SignUp = ({setLoggedIn}:LoginProps) => {
+export const SignUp = ({ setLoggedIn }: LoginProps) => {
 	const [signUpFormData, setSignUpFormData] = useState({
 		email: "",
 		password: "",
 		duplicatePassword: "",
 	});
 	const [error, setError] = useState(null);
-
+	const [loading, setLoading] = useState(false);
 	const updateSignUpFormData = (event) => {
 		const keyToUpdate = event.target.name;
 		setSignUpFormData((currentInfo) => ({
@@ -26,6 +26,7 @@ export const SignUp = ({setLoggedIn}:LoginProps) => {
 	};
 
 	const handleSubmit = async (event) => {
+		setLoading(true);
 		event.preventDefault();
 		if (validateForm()) {
 			try {
@@ -33,8 +34,8 @@ export const SignUp = ({setLoggedIn}:LoginProps) => {
 					"https://gitgoingslackbot.uc.r.appspot.com/api/auth/signup",
 					signUpFormData
 				);
-				window.localStorage.setItem("token", response.data.token)
-				setLoggedIn(true)
+				window.localStorage.setItem("token", response.data.token);
+				setLoggedIn(true);
 				setError(null);
 			} catch (err) {
 				setError(err.response.data);
@@ -45,11 +46,11 @@ export const SignUp = ({setLoggedIn}:LoginProps) => {
 					password: "",
 					duplicatePassword: "",
 				});
+				setLoading(false);
 			}
 		}
 	};
 	const validateForm = () => {
-		console.log(error);
 		if (signUpFormData.password !== signUpFormData.duplicatePassword) {
 			setError("Passwords don't match!");
 			return false;
@@ -100,13 +101,38 @@ export const SignUp = ({setLoggedIn}:LoginProps) => {
 					</div>
 					<div className="flex items-center flex-col justify-center sm:col-span-2">
 						{error ? <div className={errorMessage}>{error}</div> : null}
-						<input
-							type="submit"
-							value="Sign Up"
-							className={submitButton}
-							data-primary="black"
-							data-rounded="rounded-full"
-						/>
+						{loading ? (
+							<div
+								role="status"
+								className="flex items-center flex-col justify-center sm:col-span-2"
+							>
+								<svg
+									aria-hidden="true"
+									className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+									viewBox="0 0 100 101"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+										fill="currentColor"
+									/>
+									<path
+										d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+										fill="currentFill"
+									/>
+								</svg>
+								<span className="sr-only">Loading...</span>
+							</div>
+						) : (
+							<input
+								type="submit"
+								value="Sign Up"
+								className={submitButton}
+								data-primary="black"
+								data-rounded="rounded-full"
+							/>
+						)}
 					</div>
 				</form>
 			</div>
