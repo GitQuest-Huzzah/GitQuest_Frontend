30b3ab2ddc2bfd398e578e8a7725787e1a81b078
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-04-06 15:35:22 -0400

    maybe?

--- src/MyAccount.tsx
+++ src/MyAccount.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dashboard } from "./Dashboard";
 import { Login } from "./Login";
 
 export const MyAccount = () => {
 	const [loggedIn, setLoggedIn] = useState(false);
 	const token: string = window.localStorage.getItem("token");
+	useEffect(() => {
+	  if (token) {
+		setLoggedIn(true)
+	  }
+	}, [token])
+	
 	return (
 		<div>
 			{token && loggedIn ? (
