5c4395250d2a5cf2351aa4a50036da6b9b93a1aa
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-04-06 15:18:45 -0400

    user signup

--- src/MyAccount.tsx
+++ src/MyAccount.tsx
@@ -5,13 +5,10 @@ import { Login } from "./Login";
 export const MyAccount = () => {
 	const [loggedIn, setLoggedIn] = useState(false);
 	const token: string = window.localStorage.getItem("token");
-	useEffect(() => {
-		token ? setLoggedIn(true) : setLoggedIn(false);
-	}, [token, loggedIn]);
 
 	return (
 		<div>
-			{token ? (
+			{token && loggedIn ? (
 				<Dashboard setLoggedIn={(value) => setLoggedIn(value)} />
 			) : (
 				<Login setLoggedIn={(value) => setLoggedIn(value)} />
